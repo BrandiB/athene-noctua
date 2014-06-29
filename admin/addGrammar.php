32068fd6a1f964d09f41da510c6d0b3df2b85959
Brandi Besalke <atrejub@gmail.com>
2014-06-29 10:43:49 -0500

    Create addGrammar.php - 1st Test

---
+++ admin/addGrammar.php
@@ -0,0 +1,29 @@
+<?php
+
+include "../mysql/mysqlConnect.php";
+
+$sectionNumber = $_POST["sectionNumber"];
+$sectionText = $_POST["sectionText"];
+
+if (!$sectionNumber) {
+  // Display form to insert a section.
+
+  ?>
+    <center><h2>Add Grammar Section</h2></center><br>
+    <form action="addGrammar.php" method="post">
+    Section Number: <input type="text" name="sectionNumber"><br>
+    Section:<br>
+    <textarea rows="50" cols="400" name="sectionText">
+    <input type="submit" value="Submit">
+    </form>
+  <?php
+
+} else {
+  // Insert and display sections inserted thus far, in section order.
+  echo $sectionNumber + "<br>" + $sectionText;
+
+}
+
+mysql_close($con);
+
+?>
